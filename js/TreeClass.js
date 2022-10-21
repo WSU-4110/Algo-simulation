42a71141236c40f3ff8b9aabcd41ce09426818fe
jim-wallace <j.wallace27183@gmail.com>
2022-10-21 12:01:41 -0400

    Adding comments and cleaning up TreeClass

--- js/TreeClass.js
+++ js/TreeClass.js
@@ -1,48 +1,39 @@
 class TreeClass {
 
-  constructor(numberOfNodes)
-  {
+  constructor(numberOfNodes) {
     this.numberOfNodes = numberOfNodes;
     this.randomArray = [];
-    this.pruferSequence=[];
+    this.pruferSequence = [];
     this.copiedPrufer = [];
-    this.orderedSet =[];
-    this.treeEdges1=[];
-    this.treeEdges2=[];
-    this.treeData=[];
-    this.data=[];
-    this.root=0;
+    this.orderedSet = [];
+    this.treeEdges1 = [];
+    this.treeEdges2 = [];
+    this.treeData = [];
+    this.data = [];
+    this.root = 0;
 
   }
 
-  getNumberOfNodes()
-  {
+  getNumberOfNodes() {
     return this.numberOfNodes;
   }
 
-  setNumberOfNodes(numberOfNodes)
-  {
+  setNumberOfNodes(numberOfNodes) {
     this.numberOfNodes = numberOfNodes;
   }
 
-  getRandomInteger(max)
-  {
+  getRandomInteger(max) {
     return Math.floor(Math.random() * (max)) + 1;
   }
 
-  checkInteger(usedIntegers, newInteger)
-  {
+  checkInteger(usedIntegers, newInteger) {
     var used = false;
-    if (usedIntegers.length == 0)
-    {
+    if (usedIntegers.length == 0) {
       return used;
     }
-    else
-    {
-      for (var i = 0; i < usedIntegers.length; i++)
-      {
-        if (usedIntegers[i] == newInteger)
-        {
+    else {
+      for (var i = 0; i < usedIntegers.length; i++) {
+        if (usedIntegers[i] == newInteger) {
           used = true;
         }
       }
@@ -50,35 +41,28 @@ class TreeClass {
     }
   }
 
-  generatePruferSequence()
-  {
-    this.pruferSequence=[];
-    for(var i = 0; i < this.numberOfNodes - 2; i++)
-    {
-      this.pruferSequence[i]=this.getRandomInteger(this.numberOfNodes);
+  generatePruferSequence() {
+    this.pruferSequence = [];
+    for (var i = 0; i < this.numberOfNodes - 2; i++) {
+      this.pruferSequence[i] = this.getRandomInteger(this.numberOfNodes);
     }
   }
 
-  setRandomArray()
-  {
-    for(var i = 0; i < this.numberOfNodes; i++)
-    {
+  setRandomArray() {
+    for (var i = 0; i < this.numberOfNodes; i++) {
       this.randomArray[i] = this.getRandomInteger(this.numberOfNodes);
     }
   }
 
-  getRandomArray()
-  {
+  getRandomArray() {
     return this.randomArray;
   }
 
-  getPruferSequence()
-  {
+  getPruferSequence() {
     return this.pruferSequence;
   }
 
-  generateEdges()
-  {
+  generateEdges() {
 
     this.generatePruferSequence();
 
@@ -93,8 +77,7 @@ class TreeClass {
 
     //generating copy of prufer sequence
     console.log("Checking Copied Prufer");
-    for(var i = 0; i < this.pruferSequence.length; i++)
-    {
+    for (var i = 0; i < this.pruferSequence.length; i++) {
       this.copiedPrufer[i] = this.pruferSequence[i];
     }
 
@@ -103,37 +86,28 @@ class TreeClass {
     console.log(this.copiedPrufer);
 
 
-    //creating ordered set
-    for(var i = 0; i < this.numberOfNodes; i++)
-    {
-      this.orderedSet[i]= i + 1;
+    //creating ordered set 1 - n
+    for (var i = 0; i < this.numberOfNodes; i++) {
+      this.orderedSet[i] = i + 1;
     }
 
-    console.log("OrderedSet")
-    console.log(this.orderedSet);
-
-
-    for(var i = 0; i < this.numberOfNodes - 2; i++)
-    {
+    // 0-n-2
+    for (var i = 0; i < this.numberOfNodes - 2; i++) {
+      //u is from prufer sequence
       u = this.pruferSequence[i]
+
+
       orderedSetIndex = 0;
 
-      console.log("CopiedPrufer");
-      console.log(this.copiedPrufer);
-
-      console.log("OrderedSet")
-      console.log(this.orderedSet);
       inPruferSequence = true;
-      while(inPruferSequence)
-      {
+      //
+      while (inPruferSequence) {
 
         v = this.orderedSet[orderedSetIndex];
         inPruferSequence = false;
-        for(var j = 0; j < this.copiedPrufer.length; j++)
-        {
+        for (var j = 0; j < this.copiedPrufer.length; j++) {
 
-          if (this.copiedPrufer[j] == v)
-          {
+          if (this.copiedPrufer[j] == v) {
             inPruferSequence = true;
           }
         }
@@ -146,48 +120,39 @@ class TreeClass {
       //removes first element of prufer sequence
       this.copiedPrufer.shift();
 
-      for(var j = 0; j < this.orderedSet.length; j++)
-      {
-        console.log("Value of V")
-        console.log(v);
-        if (this.orderedSet[j] == v)
-        {
+      for (var j = 0; j < this.orderedSet.length; j++) {
+        if (this.orderedSet[j] == v) {
           this.orderedSet.splice(j, 1);
         }
       }
-
     }
-
+    //pushes last two node edges from ordered set
     this.treeEdges1.push(this.orderedSet[0]);
     this.treeEdges2.push(this.orderedSet[1]);
+
+    //selects 0th element as root
     this.root = this.orderedSet[0];
   }
 
-  getTreeEdges1()
-  {
+  getTreeEdges1() {
     return this.treeEdges1;
   }
 
-  getTreeEdges2()
-  {
+  getTreeEdges2() {
     return this.treeEdges2;
   }
 
-  returnDuplicates()
-  {
+  //checks for duplicate of nodes in first tree edge list
+  returnDuplicates() {
     var duplicates = [];
     var testValue;
     var secondTestValue;
-    for(var i = 0; i < this.treeEdges1.length -1; i++)
-    {
+    for (var i = 0; i < this.treeEdges1.length - 1; i++) {
       testValue = this.treeEdges1[i];
-      for(var j = i + 1; j < this.treeEdges1.length; j++)
-      {
+      for (var j = i + 1; j < this.treeEdges1.length; j++) {
         secondTestValue = this.treeEdges1[j];
-        if(testValue == secondTestValue)
-        {
-          if(!this.checkInteger(duplicates, testValue))
-          {
+        if (testValue == secondTestValue) {
+          if (!this.checkInteger(duplicates, testValue)) {
             duplicates.push(testValue);
           }
         }
@@ -196,57 +161,37 @@ class TreeClass {
     return duplicates;
   }
 
-  generateTree()
-  {
+  generateTree() {
 
-    this.data =[];
+    this.data = [];
     var duplicatesArray = [];
     var rootExists = false;
     var root = 0;
 
-    console.log("Here 1");
+    //ensures no duplicates in first tree edge list
+    do {
+      this.generateEdges();
+      duplicatesArray = this.returnDuplicates();
+    } while (duplicatesArray.length == 0);
 
-    while (!rootExists)
-    {
-      do {
-        this.generateEdges();
-        duplicatesArray = this.returnDuplicates();
-      } while (duplicatesArray.length == 0);
-
-      for(var i = 0; i < duplicatesArray.length; i++)
-      {
-        rootExists = true;
-        for(var j = 0; j < this.treeEdges2.length; j++)
-        {
-          if(duplicatesArray[i] == this.treeEdges2[j])
-          {
-            rootExists = false;
-          }
-        }
-        if(rootExists)
-        {
-          root = duplicatesArray[i];
-          break;
-        }
-      }
+    //creates data array of json objects to represent tree
+    for (var i = 0; i < this.treeEdges2.length; i++) {
+      this.data[i] = {
+        "name": `${this.treeEdges2[i]}`, "parent": `${this.treeEdges1[i]}`,
+        "visited": "0"
+      };
     }
 
-
-
-    for(var i = 0; i < this.treeEdges2.length; i++)
-    {
-      this.data[i] = {"name" : `${this.treeEdges2[i]}`, "parent" : `${this.treeEdges1[i]}`,
-                  "visited" : "0"};
-    }
-
-    this.data[this.treeEdges2.length] = {"name" : `${this.root}`, "parent" : "null",
-                "visited" : "0"};
+    this.data[this.treeEdges2.length] = {
+      "name": `${this.root}`, "parent": "null",
+      "visited": "0"
+    };
 
 
   }
 
 }
 
-module.exports= {
-  TreeClass:TreeClass
+module.exports = {
+  TreeClass: TreeClass
 }
